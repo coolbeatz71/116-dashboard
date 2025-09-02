eaf38ae4ff1434c7ce1977fdec40666cbd1decad
coolbeatz71 <sigmacool@gmail.com>
2025-09-03 00:57:47 +0200

    chore: setup antdesign with sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,142 @@
-import { useState } from "react";
-import reactLogo from "@/assets/react.svg";
-import "./App.css";
-import { Vite } from "@/components/common/Vite";
+import { Button, ConfigProvider, Flex } from "antd";
+import { useResponsive } from "antd-style";
+import type React from "react";
 
-function App() {
-    const [count, setCount] = useState(0);
+const App: React.FC = () => {
+    const { xxl } = useResponsive();
 
     return (
-        <>
-            <div>
-                <Vite />
-                <a href="https://react.dev" target="_blank" rel="noopener">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button
-                    type="button"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <ConfigProvider componentSize={xxl ? "middle" : "small"}>
+            <Flex
+                justify="center"
+                align="center"
+                style={{ minHeight: "100vh" }}
+            >
+                <Flex vertical gap="small">
+                    <Flex gap="small" wrap>
+                        <Button color="default" variant="solid">
+                            Solid
+                        </Button>
+                        <Button color="default" variant="outlined">
+                            Outlined
+                        </Button>
+                        <Button color="default" variant="dashed">
+                            Dashed
+                        </Button>
+                        <Button color="default" variant="filled">
+                            Filled
+                        </Button>
+                        <Button color="default" variant="text">
+                            Text
+                        </Button>
+                        <Button color="default" variant="link">
+                            Link
+                        </Button>
+                    </Flex>
+                    <Flex gap="small" wrap>
+                        <Button color="primary" variant="solid">
+                            Solid
+                        </Button>
+                        <Button color="primary" variant="outlined">
+                            Outlined
+                        </Button>
+                        <Button color="primary" variant="dashed">
+                            Dashed
+                        </Button>
+                        <Button color="primary" variant="filled">
+                            Filled
+                        </Button>
+                        <Button color="primary" variant="text">
+                            Text
+                        </Button>
+                        <Button color="primary" variant="link">
+                            Link
+                        </Button>
+                    </Flex>
+                    <Flex gap="small" wrap>
+                        <Button color="danger" variant="solid">
+                            Solid
+                        </Button>
+                        <Button color="danger" variant="outlined">
+                            Outlined
+                        </Button>
+                        <Button color="danger" variant="dashed">
+                            Dashed
+                        </Button>
+                        <Button color="danger" variant="filled">
+                            Filled
+                        </Button>
+                        <Button color="danger" variant="text">
+                            Text
+                        </Button>
+                        <Button color="danger" variant="link">
+                            Link
+                        </Button>
+                    </Flex>
+                    <Flex gap="small" wrap>
+                        <Button color="pink" variant="solid">
+                            Solid
+                        </Button>
+                        <Button color="pink" variant="outlined">
+                            Outlined
+                        </Button>
+                        <Button color="pink" variant="dashed">
+                            Dashed
+                        </Button>
+                        <Button color="pink" variant="filled">
+                            Filled
+                        </Button>
+                        <Button color="pink" variant="text">
+                            Text
+                        </Button>
+                        <Button color="pink" variant="link">
+                            Link
+                        </Button>
+                    </Flex>
+                    <Flex gap="small" wrap>
+                        <Button color="purple" variant="solid">
+                            Solid
+                        </Button>
+                        <Button color="purple" variant="outlined">
+                            Outlined
+                        </Button>
+                        <Button color="purple" variant="dashed">
+                            Dashed
+                        </Button>
+                        <Button color="purple" variant="filled">
+                            Filled
+                        </Button>
+                        <Button color="purple" variant="text">
+                            Text
+                        </Button>
+                        <Button color="purple" variant="link">
+                            Link
+                        </Button>
+                    </Flex>
+                    <Flex gap="small" wrap>
+                        <Button color="cyan" variant="solid">
+                            Solid
+                        </Button>
+                        <Button color="cyan" variant="outlined">
+                            Outlined
+                        </Button>
+                        <Button color="cyan" variant="dashed">
+                            Dashed
+                        </Button>
+                        <Button color="cyan" variant="filled">
+                            Filled
+                        </Button>
+                        <Button color="cyan" variant="text">
+                            Text
+                        </Button>
+                        <Button color="cyan" variant="link">
+                            Link
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </ConfigProvider>
     );
-}
+};
 
 export default App;
-// test hook

3e1685a55020f58af4f32ae1ee388931b0cbc82a
coolbeatz71 <sigmacool@gmail.com>
2025-09-08 01:03:25 +0200

    chore: add more elements to the architecture

---
+++ src/shared/lib/constants/paths.ts
@@ -0,0 +1,12 @@
+export const LOGIN_PATH = "/";
+export const DASHBOARD_PATH = "/dashboard";
+export const SETTING_PATH = "/settings";
+export const CONTENT_PATH = "/contents";
+export const VIDEO_PATH = "/videos";
+export const ARTICLE_PATH = "/articles";
+export const ADS_BANNER_PATH = "/ads/banners";
+export const ADS_POPUP_PATH = "/ads/popups";
+export const ADMIN_PATH = "/admins";
+export const USER_PATH = "/users";
+
+export const NOT_FOUND_PATH = "/page-introuvable";

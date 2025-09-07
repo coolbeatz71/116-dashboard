af9d9fab735c0cae6007aa738cca8679f989c093
coolbeatz71 <sigmacool@gmail.com>
2025-09-07 22:45:42 +0200

    fix: fix biomejs not catching typescript warnings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
-    "*.{jsx,js,json,css,scss,md,tsx,ts}": "biome check --write",
-    "*.{ts,tsx}": "tsc --skipLibCheck --noEmit"
+    "*.{jsx,js,json,css,scss,md,tsx,ts}": "biome check --write --error-on-warnings",
+    "*.{ts,tsx}": [() => "tsc --skipLibCheck --noEmit"]
 };

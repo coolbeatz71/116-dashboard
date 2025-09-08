938a61f2ac089b4aad1b16048cbe76edfe820c18
coolbeatz71 <sigmacool@gmail.com>
2025-09-08 22:47:55 +0200

    style: improve lottie and layout style

--- src/core/presentation/layouts/AuthLayout/AuthLayout.Loading.tsx
+++ src/core/presentation/layouts/AuthLayout/AuthLayout.Loading.tsx
@@ -1,13 +1,15 @@
-import Lottie from "lottie-react";
-import skeleton from "@/assets/lottie/skeleton_anim.json";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import skeleton from "@/assets/lottie/skeleton_anim.lottie";
 import { LottieUtils } from "@/shared/lib/utils/lottie/Lottie.Utils";
+
+import styles from "./index.module.scss";
 
 export const AuthLayoutLoading = () => {
     const lottieOptions = LottieUtils.options(skeleton);
 
     return (
-        <div className="loadingContainer">
-            <Lottie width={480} height={480} {...lottieOptions} />
+        <div className={styles.loadingContainer}>
+            <DotLottieReact className={styles.loadingContainer__lottie} {...lottieOptions} />
         </div>
     );
 };
--- src/shared/lib/utils/lottie/Lottie.Utils.ts
+++ src/shared/lib/utils/lottie/Lottie.Utils.ts
@@ -4,6 +4,13 @@ export const LottieUtils = {
     options: (animationPath: string): DotLottieReactProps => ({
         loop: true,
         autoplay: true,
-        src: animationPath
+        src: animationPath,
+        renderConfig: {
+            freezeOnOffscreen: true,
+            autoResize: true
+        },
+        layout: {
+            fit: "fill"
+        }
     })
 };

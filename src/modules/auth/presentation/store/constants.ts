f3b97b56c016584306e40f4d47461dfb7916b1d6
coolbeatz71 <sigmacool@gmail.com>
2025-10-07 01:17:47 +0200

    feat(auth): add auth store constants
    - Define action type constants
    - Add slice name constant

---
+++ src/modules/auth/presentation/store/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * Redux action type constants for auth module.
+ */
+export const ActionType = {
+    AuthLogin: "auth/login"
+};
+
+/**
+ * Redux slice name constants for auth module.
+ */
+export const SliceName = {
+    Auth: "auth"
+};
